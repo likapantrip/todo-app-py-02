ad98092e2dab135d89399a5281d436d4e24a0e51
likapantrip <likapantrip@gmail.com>
2026-06-17 03:09:28 +0900

    DB接続

---
+++ api/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# MySQLのdockerコンテナに対して接続するセッションを作成
+ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
+
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+async_session = sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
+)
+
+Base = declarative_base()
+
+# データベースセッションを取得するための依存関係
+async def get_db():
+    async with async_session() as session:
+        yield session
